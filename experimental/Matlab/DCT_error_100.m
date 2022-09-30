871cefffa1bf2d8cfb58065849382fa3abe01383
jimmyhu <60071jimmy@gmail.com>
2022-10-01 00:02:14 +0800

    Add DCT_error_100.m
    Add DCT_error_100.m

---
+++ experimental/Matlab/DCT_error_100.m
@@ -0,0 +1,39 @@
+clear all;
+clc;
+%close all;
+set(groot,'defaultFigureVisible','on');
+
+
+outputFolderRoot = '.';
+testTimes = 20000;
+
+resultsCollection = zeros(testTimes, 1);
+L1differenceCollection = zeros(testTimes, 1);
+L2differenceCollection = zeros(testTimes, 1);
+
+N = 100;
+N1 = N;
+N2 = N;
+N3 = N;
+
+test = zeros(N1, N2, N3);
+for z = 1:size(test, 3)
+    test(:, :, z) = mod(z, 2) * 255;
+end
+test_groundTruth = test;
+
+for i = 1:testTimes
+    tic;
+    test = gather(IDCT3D_gpuArray2(DCT3D_gpuArray2(gpuArray(test))));
+    L1differenceCollection(i) = sum(abs(test_groundTruth - test), 'all');
+    L2differenceCollection(i) = sqrt(sum(power(test_groundTruth - test, 2), 'all'));
+    ElapsedTime = toc;
+    resultsCollection(i) = ElapsedTime;
+    
+    % Show progress
+    fprintf('The calculation of frame %d / %d is finished!\n', i, testTimes);
+end
+
+save(fullfile(outputFolderRoot, ['DCT_error_' num2str(N) '.mat']), '-v7.3');
+
+exit;
